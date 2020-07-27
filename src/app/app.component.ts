ff8b74349a0e945581388f822aef5f2b2e9412d9
Jesus Arevalo <jarevalo@moddo.com>
2020-07-27 20:21:47 +0200

    Estructura inicial+componente de productos + model de productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from "./models/products/product.module";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+
+  
+
+
+
 }
