043a5b2bbfb9e7ca7b94fd90d875fb23694686f4
Jesus Arevalo <jarevalo@moddo.com>
2020-07-27 23:13:23 +0200

    Paso a detalle de producto y su servicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { ProductsComponent } from "./products/products.component";
+import { ProductDetailComponent } from "./products/product-detail/product-detail.component";
 import { ContactComponent } from "./contact/contact.component";
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -21,6 +22,10 @@ const routes: Routes = [
     component: ProductsComponent
   },
   {
+    path: 'products/:id',
+    component: ProductDetailComponent
+  },
+  {
     path: 'contact',
     component: ContactComponent
   },
@@ -29,7 +34,7 @@ const routes: Routes = [
     component: DemoComponent
   },
   {
-    path:'**',
+    path: '**',
     component: PageNotFoundComponent
   }
 ];
