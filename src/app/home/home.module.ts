d866fe13c5105f1e02b7effd6c452ddfbdc584da
Jesus Arevalo <jarevalo@moddo.com>
2020-08-02 20:56:27 +0200

    Shared module y coreModule

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './components/banner/banner.component';
 import { HomeComponent } from './components/home/home.component';
-import {  HomeRoutingModule } from './home-routing.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -11,7 +12,8 @@ import {  HomeRoutingModule } from './home-routing.module';
     ],
     imports: [
         HomeRoutingModule,
-        CommonModule
+        CommonModule,
+        SharedModule
     ],
     providers: [],
     bootstrap: []
