3e2979eaff9567c1eaf69da9a0bb2fe8b084f5d6
Jesus Arevalo <jarevalo@moddo.com>
2020-08-04 22:29:43 +0200

    angular material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HighlightDirective } from './directives/highlight/highlight.directive';
-import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 
+import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
+import { HighlightDirective } from './directives/highlight/highlight.directive';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    FooterComponent,
-    HeaderComponent,
+    ExponentialPipe,
     HighlightDirective,
-    ExponentialPipe
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    ExponentialPipe,
+    HighlightDirective,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     RouterModule
-  ],
-  exports: [
-    FooterComponent,
-    HeaderComponent,
-    HighlightDirective,
-    ExponentialPipe
   ]
 })
 export class SharedModule { }
